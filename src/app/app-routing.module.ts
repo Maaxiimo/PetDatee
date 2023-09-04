f2792eb7b7df2b34216ce5ee7e55f62b70c64e43
Axloon <ax.rojasc@duocuc.cl>
2023-09-04 20:05:00 -0300

    page profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,6 +18,10 @@ const routes: Routes = [
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
   },
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
+  },
 
 ];
 
