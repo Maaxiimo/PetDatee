6651587ddab4d2e4521f9fea5aacd7d8d09f33c6
Axloon <ax.rojasc@duocuc.cl>
2023-09-05 18:25:03 -0300

    validacion login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  templateUrl: 'login.page.html',
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
 
-  constructor() { }
+  formulario: FormGroup;
 
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {
+    this.formulario = this.formBuilder.group({
+      contrasena: [
+        'password',
+        [Validators.required, this.validarContrasena]
+      ]
+    });
   }
 
+  validarContrasena(control: AbstractControl): { [key: string]: boolean } | null {
+    const value = control.value;
+    if (value && value.length >= 8 && /[A-Z]/.test(value) && /\d/.test(value)) {
+      return null;
+    } else {
+      return { contrasenaInvalida: true };
+    }
+  }
+
+  enviarFormulario() {
+    // Agrega aquí el código para manejar el envío del formulario
+  }
 }
