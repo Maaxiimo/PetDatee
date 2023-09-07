dab69b84554ade357e2394f3dc277c69bd148abd
Axloon <ax.rojasc@duocuc.cl>
2023-09-06 23:14:40 -0300

    animation home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,9 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChildren, ViewChild } from '@angular/core';
 import { IonModal } from '@ionic/angular';
 import { OverlayEventDetail } from '@ionic/core/components';
+import type { QueryList } from '@angular/core';
+import type { Animation } from '@ionic/angular';
+import { AnimationController, IonCard } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +11,27 @@ import { OverlayEventDetail } from '@ionic/core/components';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild(IonCard, { read: ElementRef })
+  card!: ElementRef<HTMLIonCardElement>;
   isModalOpen = false;
 
-  constructor() {}
+  private animation!: Animation;
 
+  constructor(private animationCtrl: AnimationController) {}
+
+  ngAfterViewInit() {
+    this.animation = this.animationCtrl
+      .create()
+      .addElement(this.card.nativeElement)
+      .duration(3000)
+      .direction('alternate')
+      .fromTo('background', 'red', 'var(--background)');
+  }
+
+  play() {
+    this.animation.play();
+  }
+  
   setOpen(isOpen: boolean) {
     this.isModalOpen = isOpen;
   }
