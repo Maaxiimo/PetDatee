5c599c4620367a4d53a7885c8204ed41e6bb48fa
Axloon <ax.rojasc@duocuc.cl>
2023-11-09 23:02:15 -0300

    validaciones rdn

--- src/app/add/add.page.ts
+++ src/app/add/add.page.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.page.html',
   styleUrls: ['./add.page.scss'],
 })
-export class AddPage implements OnInit {
+export class AddPage {
 
-  constructor() { }
+  petForm: FormGroup;
 
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {
+    this.petForm = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      edad: ['', Validators.required],
+      raza: ['', Validators.required],
+      sexo: ['', Validators.required],
+      descripcion: ['', Validators.required],
+    });
   }
 
+  onSubmit() {
+    if (this.petForm.valid) {
+      // Aquí puedes agregar lógica adicional, como enviar datos a un servidor.
+      console.log('Formulario válido', this.petForm.value);
+    } else {
+      console.log('Formulario inválido');
+      // Puedes mostrar mensajes de error o realizar otras acciones aquí
+    }
+  }
 }
