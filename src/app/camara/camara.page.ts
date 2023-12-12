a8d02c979e29f23fb9517ecb74b11c8abe714113
Axloon <ax.rojasc@duocuc.cl>
2023-12-12 00:24:31 -0300

    cmbs

--- src/app/camara/camara.page.ts
+++ src/app/camara/camara.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { PhotoService } from '../services/photo.service';
+import { PhotoService, UserPhoto } from '../services/photo.service';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-camara',
@@ -8,12 +9,36 @@ import { PhotoService } from '../services/photo.service';
 })
 export class CamaraPage implements OnInit {
 
-  constructor(public photoService: PhotoService) { }
+  constructor(public photoService: PhotoService, public actionSheetController: ActionSheetController) { }
 
-addPhotoToGallery() {
-  this.photoService.addNewToGallery();
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
 }
-  ngOnInit() {
+
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
+
+  public async showActionSheet(photo: UserPhoto, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    });
+    await actionSheet.present();
   }
 
 }
