5899c044a6c4a0a35cbe0dac03221aec2b441473
Axloon <ax.rojasc@duocuc.cl>
2023-12-12 00:52:03 -0300

    Revert "cmbs"
    This reverts commit a8d02c979e29f23fb9517ecb74b11c8abe714113.

--- src/app/camara/camara.page.ts
+++ src/app/camara/camara.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { PhotoService, UserPhoto } from '../services/photo.service';
-import { ActionSheetController } from '@ionic/angular';
+import { PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-camara',
@@ -9,36 +8,12 @@ import { ActionSheetController } from '@ionic/angular';
 })
 export class CamaraPage implements OnInit {
 
-  constructor(public photoService: PhotoService, public actionSheetController: ActionSheetController) { }
+  constructor(public photoService: PhotoService) { }
 
-  addPhotoToGallery() {
-    this.photoService.addNewToGallery();
+addPhotoToGallery() {
+  this.photoService.addNewToGallery();
 }
-
-  async ngOnInit() {
-    await this.photoService.loadSaved();
-  }
-
-  public async showActionSheet(photo: UserPhoto, position: number) {
-    const actionSheet = await this.actionSheetController.create({
-      header: 'Photos',
-      buttons: [{
-        text: 'Delete',
-        role: 'destructive',
-        icon: 'trash',
-        handler: () => {
-          this.photoService.deletePicture(photo, position);
-        }
-      }, {
-        text: 'Cancel',
-        icon: 'close',
-        role: 'cancel',
-        handler: () => {
-          // Nothing to do, action sheet is automatically closed
-          }
-      }]
-    });
-    await actionSheet.present();
+  ngOnInit() {
   }
 
 }
